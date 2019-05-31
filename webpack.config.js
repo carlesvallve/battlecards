d9b545e3472fcb7302d83ad15aff6e3cf9afb590
Carles Vallve <snaptothegrid@gmail.com>
2019-05-31 12:36:57 +0900

    prettier

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,19 @@ const path = require('path');
 var webpackConfig = require('@blackstormlabs/timestep-build/arson/webpack.config.js');
 
 // This is what webpack actually calls.
-module.exports = function (env) {
+module.exports = function(env) {
   env.IS_DEVELOPMENT = env.buildType === 'development';
 
   // First run the real config script.
   var config = webpackConfig(env);
 
   // clientSchema entry point is required for gcf-deploy
-  config.entry['clientSchema'] = path.resolve(process.cwd(), 'src', 'stormcloud', 'clientSchema');
+  config.entry['clientSchema'] = path.resolve(
+    process.cwd(),
+    'src',
+    'stormcloud',
+    'clientSchema',
+  );
 
   // If you need to alter the config you can do so here.
 
